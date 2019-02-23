5069b03c012a65c5e4eb25d9997512ed34dbfebe
阿柴 <wwyg1213@163.com>
2019-02-23 20:28:05 +0800

    中间件：next 传参、错误处理

--- test/2. middleware.js
+++ test/2. middleware.js
@@ -3,8 +3,9 @@ const express = require('../lib/express');// 自定义库
 const app = express();
 
 app.use((req, res, next) => {
+    res.setHeader('Content-type', 'text/html;charset=utf8');
     console.log('无路径中间件');
-    next();
+    next('报错');
 });
 app.use('/moon', (req, res, next) => {
     console.log('有路径中间件');
@@ -13,6 +14,11 @@ app.use('/moon', (req, res, next) => {
 
 app.get('/moon', (req, res) => {
     res.end('moon');
+});
+
+app.use((err, req, res, next) => {
+    console.log('错误处理中间件：' + err)
+    res.end('system error');
 });
 
 app.listen(8080, _ => { console.log('8080 端口服务器启动')})
