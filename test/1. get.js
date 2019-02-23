d7ff0417ce351be0b0a81c1b0eeeeda786d0db7e
阿柴 <wwyg1213@163.com>
2019-02-23 09:41:26 +0800

    基本功能： get 设定路由

---
+++ test/1. get.js
@@ -0,0 +1,15 @@
+// const express = require('express'); // 标准库
+const express = require('../lib/express');// 自定义库
+const app = express();
+
+// express 重要功能：路由
+// 通过设定路由规则，可以根据不同的请求方法和路径，响应不同内容
+
+app.get('/moon', (req, res) => {
+    res.end('moon');
+});
+app.get('/wang', (req, res) => {
+    res.end('wang');
+});
+
+app.listen(8080, () => console.log('启动8080端口服务器'));
